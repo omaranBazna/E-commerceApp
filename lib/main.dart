e143515e876200a733c0c9b345761bc514cf2f66
Omaran_Bazna <omran4d@gmail.com>
2023-11-24 00:08:55 +0000

    add connection to login

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,151 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
-import "package:helloworld/screens/HomeScreen/HomeScreen.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // Application name
-        title: 'Flutter Hello World',
-        // Application theme data, you can set the colors for the application as
-        // you want
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: AuthPage(),
+    );
+  }
+}
+
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String email = "";
+  String password = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+                decoration: InputDecoration(hintText: 'Email'),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                }),
+            SizedBox(height: 20.0),
+            TextField(
+                decoration: InputDecoration(hintText: 'Password'),
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                }),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: () {
+                // check if the email exist
+                authUser();
+                /* Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => HomeScreen()));*/
+              },
+            )
+          ],
         ),
-        // A widget which will be started on application startup
-        home: HomeScreen());
+      ),
+    );
+  }
+
+  void authUser() async {
+    try {
+      var url = Uri.parse('https://bendapi.omranbazna.repl.co/login');
+
+      var response = await http.post(url, headers: {
+        'Content-Type': 'application/json',
+      }, body: {
+        "email": email,
+        "password": password,
+      });
+      print(response.body);
+    } catch (error) {
+      print("Error: $error");
+    }
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Page1',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Page2'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Page3')
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 1 Content'),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 2 Content'),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page 3 Content'),
+    );
   }
 }
